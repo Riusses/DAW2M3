38917080050dd262b5ab4de121376666007803f1
Riusses <riusses@rius.com>
2015-11-13 16:54:51 +0100

    Push a GITHUB.

--- Peixera/src/net/Peixera/Peixera.java
+++ Peixera/src/net/Peixera/Peixera.java
@@ -8,7 +8,7 @@ public class Peixera {
 
 	Random Aleatori = new Random();
 	
-	// CREACIÓ DE LA LLISTA DE PEIXOS
+	// MÈTODE CREACIÓ DE LA LLISTA DE PEIXOS
 
 	List<Peix> Peixos = new ArrayList<Peix>();
 	int Acaba = Peixos.size();
@@ -30,26 +30,24 @@ public class Peixera {
 		}
 	}
 	
+	// MÈTODE DE IF DE REPETICIÓ
+	
 	public void peixeraParet(Peix P) {
-		// IF DE REPETICIO
 		if (P.tamanyPeix().getX() > 800) {
 			int temp1 = (int) P.tamanyPeix().getX() + 1;
 			int temp2 = temp1 % 800;
 			int temp3 = temp2 - P.ampladaPeix();
 			P.posicioPeix(temp3, (int) P.tamanyPeix().getY());
 		}
-		// IF DE REPETICIO 
 		if (P.tamanyPeix().getY() > 600) {
 			int temp1 = (int) P.tamanyPeix().getY() + 1;
 			int temp2 = temp1 % 600;
 			int temp3 = temp2 - P.alturaPeix();
 			P.posicioPeix((int) P.tamanyPeix().getX(), temp3);
 		}
-		// IF DE REPETICIO 
 		if (P.tamanyPeix().getX() < 0 - P.ampladaPeix()) {
 			P.posicioPeix(800, (int) P.tamanyPeix().getY());
 		}
-		// IF DE REPETICIO 
 		if (P.tamanyPeix().getY() < 0 - P.alturaPeix()) {
 			P.posicioPeix((int) P.tamanyPeix().getX(), 600);
 		}
